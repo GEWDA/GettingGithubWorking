4d429587cbe3418a5d73da3705b20e8b2f2ccbf4
Judah Parsons <GEWDDEWD9@hotmail.com>
2017-03-21 14:28:47 +1300

    disabled the keyboard (except for Win + L and Ctrl + Alt + Del)

--- GettingGithubWorking/Form1.cs
+++ GettingGithubWorking/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace GettingGithubWorking
 {
@@ -18,15 +19,88 @@ namespace GettingGithubWorking
             this.TopMost = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+            KeyboardHook(this, e); //copied from internet
         }
         public Form1()
         {
             InitializeComponent();
         }
-        
 
+        //copied from : http://stackoverflow.com/questions/18213150/disable-keyboard-using-c-sharp
 
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int
+          wParam, ref KBDLLHOOKSTRUCT lParam);
 
+        [DllImport("user32.dll", EntryPoint = "SetWindowsHookExA", CharSet = CharSet.Ansi)]
+        private static extern int SetWindowsHookEx(
+           int idHook,
+           LowLevelKeyboardProcDelegate lpfn,
+           int hMod,
+           int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern int UnhookWindowsHookEx(int hHook);
+
+        [DllImport("user32.dll", EntryPoint = "CallNextHookEx", CharSet = CharSet.Ansi)]
+        private static extern int CallNextHookEx(
+            int hHook, int nCode,
+            int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        const int WH_KEYBOARD_LL = 13;
+        private int intLLKey;
+        private KBDLLHOOKSTRUCT lParam;
+
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            int scanCode;
+            public int flags;
+            int time;
+            int dwExtraInfo;
+        }
+
+        private int LowLevelKeyboardProc(
+            int nCode, int wParam,
+            ref KBDLLHOOKSTRUCT lParam)
+        {
+            bool blnEat = false;
+            switch (wParam)
+            {
+                case 256:
+                case 257:
+                case 260:
+                case 261:
+                    //Alt+Tab, Alt+Esc, Ctrl+Esc, Windows Key
+                    if (((lParam.vkCode == 9) && (lParam.flags == 32)) ||
+                    ((lParam.vkCode == 27) && (lParam.flags == 32)) || ((lParam.vkCode ==
+                    27) && (lParam.flags == 0)) || ((lParam.vkCode == 91) && (lParam.flags
+                    == 1)) || ((lParam.vkCode == 92) && (lParam.flags == 1)) || ((true) &&
+                    (lParam.flags == 32)))
+                    {
+                        blnEat = true;
+                    }
+                    break;
+            }
+
+            if (blnEat)
+                return 1;
+            else return CallNextHookEx(0, nCode, wParam, ref lParam);
+
+        }
+
+        private void KeyboardHook(object sender, EventArgs e)
+        {
+            intLLKey = SetWindowsHookEx(WH_KEYBOARD_LL, new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc),
+                       System.Runtime.InteropServices.Marshal.GetHINSTANCE(
+                       System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]).ToInt32(), 0);
+        }
+
+        private void ReleaseKeyboardHook()
+        {
+            intLLKey = UnhookWindowsHookEx(intLLKey);
+        }
+
+        //end of copied code
 
         //disables the minimize, maximize, and exit buttons
         private const int WS_SYSMENU = 0x80000;
